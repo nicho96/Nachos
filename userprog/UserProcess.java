ea35a6030db839b46a9fba2ebe92c65752251de3
Paul Tonon <paultonon@Pauls-MacBook-Pro.local>
2018-04-04 23:01:45 -0400

    NOT 100% sure about this..2.0

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -369,6 +369,7 @@ public class UserProcess {
 	    return false;
 	}
 	if( numPages <= 8 && ((UserKernel)Kernel.kernel).isAvailable(numPages) ){
+		System.out.println("Num Pages: " + numPages);
 	pageLock.acquire();
 	pageTable = ((UserKernel)Kernel.kernel).getPages(numPages);
 	for(int i = 0; i < pageTable.length; i++)
