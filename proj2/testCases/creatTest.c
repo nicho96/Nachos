e2dff9a8e26ba06e167e8d121d17bd5aca587fc8
Collin Marando <collin.marando@hotmail.com>
2018-04-07 00:29:10 -0400

    multi create

--- proj2/testCases/creatTest.c
+++ proj2/testCases/creatTest.c
@@ -2,15 +2,18 @@
 int main(int argc, char* argv[]){
   printf("----TESTING CREATE----\n");
   
-  printf("Creating a file called \"creatTestFile\"\n");
-  int fileDescriptor = creat("creatTestFile");
+  printf("Creating a file called \"creatTestFile1\"\n");
+  int fileDescriptor1 = creat("creatTestFile1");
   
-  printf("Checking whether a valid file descriptor was returned\n");
-  if(fileDescriptor != -1){
-		printf("Success: file created\nFile Descriptor: %d\n\n",fileDescriptor);
+  printf("Creating a file called \"creatTestFile2\"\n");
+  int fileDescriptor2 = creat("creatTestFile2");
+  
+  printf("Checking whether valid file descriptors were returned\n");
+  if(fileDescriptor1 != -1 && fileDescriptor2 != -1){
+		printf("Success: files created\nFile Descriptor for createTestFile1: %d\nFile Descriptor for createTestFile2: %d\n\n",fileDescriptor1,fileDescriptor2);
 		return 0;
 	}
-  printf("Failure: file not created\n\n");
+  printf("Failure: files not created\n\n");
 	return -1;
   
 }
