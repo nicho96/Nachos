77ad9b3555e2adb9cb2b733121fdba7cebe72447
Collin Marando <collin.marando@hotmail.com>
2018-04-06 20:19:35 -0400

    readTest

--- proj2/testCases/readTest.c
+++ proj2/testCases/readTest.c
@@ -8,7 +8,10 @@ int main(int argc, char* argv[]){
 	int bytesRead = read(fileDescriptor, buffer, 6);
   
 	if(bytesRead != -1){
-		printf("Success: file was read\n", bytesWritten);
+		printf("Success: file was read: ");
+    for(int i = 0; i < 6; i++)
+      printf("%c", buffer[i]);
+    printf("\n");
 		return 0;
 	}
   printf("Failure: file was not read\n");
